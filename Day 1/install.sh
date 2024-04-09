b0479b9093da60047eac7f157ab19a79a0608603
Antônio Neto <soh.tecnologia@gmail.com>
2024-04-09 16:12:43 -0300

    first commit

---
+++ Day 1/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+
+chmod +x ./kind
+
+sudo mv ./kind /usr/local/bin/kind
